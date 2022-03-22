381a6e251e5d885cf4d3bf04a4c12b14f39babb2
Sakthi-KEC <97332413+Sakthi-KEC@users.noreply.github.com>
2022-03-22 18:40:43 +0530

    Update RsCommodityGroupController.java

--- src/main/java/net/microservices/RsCommodityGroup/controller/RsCommodityGroupController.java
+++ src/main/java/net/microservices/RsCommodityGroup/controller/RsCommodityGroupController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping("/commoditygroup")
 
 public class RsCommodityGroupController
